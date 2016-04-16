4e9fbce9e6d52fe3088ed964f3b2128b265f429c
jumaanji <cadarm_z@etna-alternance.net>
2016-04-16 13:03:05 +0200

    no routes

--- controller/indexCtrl.js
+++ controller/indexCtrl.js
@@ -2,6 +2,7 @@
 
 exports.getIndex = function (req, res) {
     // clean previous session result
+    //connexion à bdd sql 
 
     return res.render('index', {title: "Home", template: "home.ejs"});
 };
